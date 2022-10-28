b367d52770ad854856d63d755598495bf1008a5a
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-10-28 09:25:13 +0100

    Use centralzed build logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
 plugins {
-    java
-    id("io.quarkus")
+    id("org.orph2020.pst.common-plugin")
 }
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-}
-
-val quarkusPlatformGroupId: String by project
-val quarkusPlatformArtifactId: String by project
-val quarkusPlatformVersion: String by project
+version = "0.1"
 
 dependencies {
-    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
     implementation("io.quarkus:quarkus-rest-client-reactive-jackson")
     implementation("io.quarkiverse.quinoa:quarkus-quinoa:1.2.2")
@@ -21,19 +12,6 @@ dependencies {
     implementation("io.quarkus:quarkus-smallrye-jwt")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy-reactive")
-    testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
 
-group = "org.orph2020.pst"
-version = "0.1"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,4 @@
 pluginManagement {
-    val quarkusPluginVersion: String by settings
-    val quarkusPluginId: String by settings
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        mavenLocal()
-    }
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
+    includeBuild("../build-logic")
 }
 rootProject.name="pst-gui"
