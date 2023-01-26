6caf6c44f2b0753f6030406929faf41119270580
Allan England <allan.england@manchester.ac.uk>
2023-01-26 17:29:52 +0000

    Another rejig, use react-router-dom v6 and change routing

--- src/main/webui/src/index.js
+++ src/main/webui/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
