059522de6bc50df16334bc9da4909788e83ebb5a
Allan England <allan.england@manchester.ac.uk>
2023-03-01 12:27:28 +0000

    Fix more merge conflicts

--- src/main/webui/src/App.js
+++ src/main/webui/src/App.js
@@ -6,6 +6,7 @@ import { Dropdown, Menu } from 'semantic-ui-react';
 import {confirmAlert} from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
+
 export default function App() {
     const [loggedIn, setLoggedIn] = React.useState(false);
     const [activeItem, setActiveItem] = React.useState("logout");
