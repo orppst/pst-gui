c94c3c8ff158965be74d20eb5a8f59d360729549
Allan England <allan.england@manchester.ac.uk>
2023-03-01 12:18:48 +0000

    Fix merge conflicts

---
+++ src/main/webui/src/ProposalApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import NameListApp from './NameList'
+
+class ProposalApp extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Proposal tool front end</h1>
+            <NameListApp />
+            </div>
+        );
+  }
+}
+
+
+export default ProposalApp;
