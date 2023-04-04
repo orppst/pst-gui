79abd3c2bcc28793b7e9d51e14be9c463f984f4c
Allan England <allan.england@manchester.ac.uk>
2023-04-04 11:16:37 +0100

    Display user's home institute details

--- src/main/webui/src/user/Edit.js
+++ src/main/webui/src/user/Edit.js
@@ -10,47 +10,55 @@ const schema: RJSFSchema = {
         type: "object",
         "properties": {
             "fullName": { type: "string", title: "Full Name", default: "Your name" },
-            "eMail": { type: "string", title: "email address", default: "you@example.com" }
+            "eMail": { type: "string", title: "email address", default: "you@example.com" },
+            "homeInstitute": {
+            type: "object",
+            title: "Home institute",
+                "properties": {
+                    instituteName: { type: "string", title: "Name", default: "none", readOnly: true },
+                    instituteAddr: { type: "string", title: "Address", default: "none", readOnly: true },
+                }
+            }
         }
     };
+
+const uiSchema = { };
 
 var person = {};
 const response = fetch(window.location.pathname + '/proposalapi/people/37').then(res=> res.json()).then((data) => {person = data;});
 
 const onSubmit = ({formData}, e) => {
     console.log("Data submitted: ",  formData);
+
     //Map back into person object
-    person.fullName = formData.fullName;
-    person.eMail = formData.eMail;
-    console.log("New Person Data: ", person);
-    //Update name
-    const requestOptions = { method: 'PUT', body: person.fullName };
-    fetch(window.location.pathname + '/proposalapi/people/37/fullName', requestOptions);
-    //Update email
-    const requestOptsEmail = { method: 'PUT', body: person.eMail };
-    fetch(window.location.pathname + '/proposalapi/people/37/eMail', requestOptsEmail);
+    if(person.fullName != formData.fullName) {
+        person.fullName = formData.fullName;
+        //Update name
+        const requestOptions = { method: 'PUT', body: person.fullName };
+        fetch(window.location.pathname + '/proposalapi/people/37/fullName', requestOptions);
+    }
+
+    if(person.eMail !=   formData.eMail) {
+        person.eMail = formData.eMail;
+        //Update email
+        const requestOptsEmail = { method: 'PUT', body: person.eMail };
+        fetch(window.location.pathname + '/proposalapi/people/37/eMail', requestOptsEmail);
+    }
 
 }
 
 export default function EditUser() {
-/*
-    fetch(window.location.pathname + '/proposalapi/people')
-        .then(res => res.json())
-        .then((data) => {console.log(data);})
-        .catch(console.log);
 
-    fetch(window.location.pathname + '/proposalapi/people/37')
-        .then(res => res.json())
-        .then((data) => {person = data;})
-        .catch(console.log);
-*/
     schema.properties.fullName.default = person.fullName;
     schema.properties.eMail.default = person.eMail;
+    schema.properties.homeInstitute.properties.instituteName.default = person.homeInstitute.name;
+    schema.properties.homeInstitute.properties.instituteAddr.default = person.homeInstitute.address;
 
     return (
            <div className="Prop-form-container">
                 <Form className="Prop-form"
                      schema={schema}
+                     uiSchema={uiSchema}
                      validator={validator}
                      onSubmit={onSubmit}
                      onError={log("errors")} />
