6060a22daa137b9807746b9143b1a986e4233eb2
Darren Walker <darrenwalker1@hotmail.co.uk>
2023-07-14 16:51:37 +0100

    add a 'Target' section to the new 'Observation' form allowing to input a target name, coordinates, space frame and position epoch.

--- src/main/webui/src/observations/New.tsx
+++ src/main/webui/src/observations/New.tsx
@@ -4,11 +4,19 @@ import {formReducer} from "../App2";
 function NewObservationPanel() {
 
     const [formData, setFormData] =
-        useReducer(formReducer, {observationType:"Target", calibrationUse:"placeHolder"});
+        useReducer(formReducer, {
+            observationType:"Target",
+            calibrationUse:"placeHolder",
+            targetName: "",
+            targetLongitude: "",
+            targetLatitude: "",
+            targetSpaceFrame:"placeHolder",
+            targetPositionEpoch:"placeHolder"
+        });
 
     function handleCreate(event: React.SyntheticEvent) {
         event.preventDefault();
-        console.log("create pressed") //TODO: call the generated fetch(?) function to the API
+        console.log(formData) //TODO: call the generated fetch(?) function to the API
     }
 
     function handleChange(event : React.SyntheticEvent) {
@@ -17,6 +25,35 @@ function NewObservationPanel() {
             name: event.target.name,
             value: event.target.value,
         });
+    }
+
+
+
+    //The input type number allows for scientific notation
+    // i.e., '[+,-] <mantissa> [e,E] [+,-] <exponent>'
+    // restricting the input to contain a maximum of two sign characters and one 'e/E' character.
+    // However, there are seemingly no restrictions on WHERE the '+/-' or 'e/E' characters can be inserted
+    // into the input, with the exception that character following the 'e/E' must be a sign or a numeric and
+    // after that character no more sign characters can be used. This means we could receive the following
+    // input:
+    // '+', '++', '-', '--', 'e+', 'e-', '+e+', '-e-', '<N>-<M>-<P>' plus other variations of these.
+    //
+    // For example, an input of '123-45+678' can be passed but is clearly meaningless in this context.
+    function handleCoordinateChange (event : React.SyntheticEvent) {
+        const {name, value, min, max} = event.target;
+
+        // check for null value such that coordinate isn't set to zero, which prevents the user
+        // from entering a sign symbol on subsequent input
+        const coordinate = !value ? "" : Math.max(min, Math.min(max, value)).toString();
+
+        //regex: optional +- then any number of digits finally limit to five decimal places if any
+        if (!coordinate || coordinate.match(/^[-+]?\d+(\.\d{0,5})?$/)) {
+            setFormData({
+                // @ts-ignore
+                name: event.target.name,
+                value: coordinate,
+            });
+        }
     }
 
     function DisplayObservationType() {
@@ -50,7 +87,8 @@ function NewObservationPanel() {
         return (
             <div className={"form-group"}>
                 <label>Calibration intended use:</label>
-                <select defaultValue={formData.calibrationUse} className={"form-control"} name={"calibrationUse"} onChange={handleChange}>
+                <select defaultValue={formData.calibrationUse} className={"form-control"}
+                        name={"calibrationUse"} onChange={handleChange}>
                     <option value="placeHolder" disabled hidden>
                         --calibration intended use--
                     </option>
@@ -77,9 +115,70 @@ function NewObservationPanel() {
     }
 
     function DisplayTarget() {
+
+        // source name - string
+        // source coordinates - longitude, latitude, space frame
+        // position epoch e.g., J2000.0
+
+        function TargetName() {
+            return (
+                <div className={"form-group"}>
+                    <label>Target name</label>
+                    <input className={"form-control"} name={"targetName"} onChange={handleChange} />
+                </div>
+            )
+        }
+
+        function TargetCoordinates() {
+            return (
+                <div className={"form-group"}>
+                    <label>longitude in degrees, range [-180.,+180.]</label>
+                    <input className={"form-control"} name={"targetLongitude"} type="number"
+                           min="-180.0" max="180.0"
+                           value={formData.targetLongitude}
+                           onChange={handleCoordinateChange} />
+
+                    <label>latitude in degrees, range [-90.,+90.]</label>
+                    <input className={"form-control"} name={"targetLatitude"} type="number"
+                           min="-90.0" max="90.0"
+                           value={formData.targetLatitude}
+                           onChange={handleCoordinateChange} />
+
+                    <label>space frame</label>
+                    <select defaultValue={formData.targetSpaceFrame} className={"form-control"}
+                            name={"targetSpaceFrame"} onChange={handleChange}>
+                        <option value="placeHolder" disabled hidden>
+                            --target space frame--
+                        </option>
+                        <option value="ICRS">ICRS</option>
+                        <option value={"GEOCENTRIC"}>Geocentric</option>
+                    </select>
+                </div>
+            )
+        }
+
+        function TargetPositionEpoch() {
+            return (
+                <div className={"form-group"}>
+                    <label>position epoch</label>
+                    <select defaultValue={formData.targetPositionEpoch} className={"form-control"}
+                            name={"targetPositionEpoch"} onChange={handleChange}>
+                        <option value="placeHolder" disabled hidden>
+                            --target position epoch--
+                        </option>
+                        <option value="J2000.0">J2000.0</option>
+                        <option value="B1950.0">B1950.0</option>
+                    </select>
+                </div>
+            )
+        }
+
         return (
-            <div className={"form-group"}>
-                Target selection here<br/>
+            <div>
+                Specify a target:<br />
+                {TargetName()}
+                {TargetCoordinates()}
+                <TargetPositionEpoch />
             </div>
         )
     }
@@ -101,6 +200,7 @@ function NewObservationPanel() {
     }
 
 
+    //curly brace call is required due to how React triggers a render of a form
     return (
         <div className={""}>
             <h3>Create an Observation</h3>
@@ -113,7 +213,7 @@ function NewObservationPanel() {
 
                 <DisplayTimingWindow/>
 
-                <DisplayTarget/>
+                {DisplayTarget()}
 
                 <DisplayField/>
 
