3ff79632a77bd39de3347c178cfaafa51cad33b2
Allan England <allan.england@manchester.ac.uk>
2023-07-14 14:31:06 +0100

    Return main.tsx to previous content

--- src/main/webui/src/main.tsx
+++ src/main/webui/src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App2 from "./App2.tsx"
 import './index.css'
-//import {DevSupport} from "@react-buddy/ide-toolbox";
-//import {ComponentPreviews, useInitial} from "./dev";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
             <App2/>
+        </DevSupport>
     </React.StrictMode>,
 )
