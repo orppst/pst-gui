a19e6860fe748a1a2b1e373f0fa476e0e1e11394
astokes <alan.stokes@stfc.ac.uk>
2023-11-03 14:32:01 +0000

    mini clean up

--- src/main/webui/src/observations/observationPanel.tsx
+++ src/main/webui/src/observations/observationPanel.tsx
@@ -1,13 +1,13 @@
 import {
     useObservationResourceGetObservations, useProposalResourceGetObservingProposalTitle, useProposalResourceGetTargets,
 } from "../generated/proposalToolComponents";
-import ObservationsNewModal from "./new.modal.tsx";
 import {Link, useParams} from "react-router-dom";
 import ObservationRow, { observationTableHeader } from './observationTable.tsx';
 import {Badge, Button, Group, Space, Table, Text} from "@mantine/core";
 import {Observation} from "../generated/proposalToolSchemas.ts";
 import getErrorMessage from "../errorHandling/getErrorMessage.tsx";
 import { ReactElement } from 'react';
+import ObservationEditModal from './edit.modal.tsx';
 
 
 /**
@@ -20,7 +20,7 @@ import { ReactElement } from 'react';
  */
 export type ObservationProps = {
     observation: Observation | undefined,
-    observationId: number, // needed as 'observation' does not contain its database id
+    observationId?: number, // needed as 'observation' does not contain its database id
     newObservation?: boolean, // this might be redundant i.e. observation === undefined contains the information
     closeModal?: () => void
 }
@@ -37,106 +37,105 @@ export type ObservationProps = {
  * @constructor
  */
 function ObservationsPanel(): ReactElement {
-
     return (
         <>
-            <Observations />
+            <Observations/>
         </>
     );
+}
 
-    //reminder we are getting lists of 'ObjectIdentifiers' which contain only a name and DB id for
-    //the object specified i.e. we don't get any information on child objects.
+//reminder we are getting lists of 'ObjectIdentifiers' which contain only a name and DB id for
+//the object specified i.e. we don't get any information on child objects.
+function Observations() {
+    const { selectedProposalCode} = useParams();
 
-    function Observations() {
-        const { selectedProposalCode} = useParams();
-
-        const { data: observations , error: observationsError, isLoading: observationsLoading } =
-            useObservationResourceGetObservations(
-                {pathParams: {proposalCode: Number(selectedProposalCode)},},
-                {enabled: true}
-            );
+    const { data: observations , error: observationsError, isLoading: observationsLoading } =
+        useObservationResourceGetObservations(
+            {pathParams: {proposalCode: Number(selectedProposalCode)},},
+            {enabled: true}
+        );
 
 
-        const {data: targets, error: targetsError, isLoading: targetsLoading} =
-            useProposalResourceGetTargets({pathParams: {proposalCode: Number(selectedProposalCode)}},
-                {enabled: true}
-            );
+    const {data: targets, error: targetsError, isLoading: targetsLoading} =
+        useProposalResourceGetTargets({pathParams: {proposalCode: Number(selectedProposalCode)}},
+            {enabled: true}
+        );
 
-        const {data: titleData, error: titleError, isLoading: titleLoading} =
-            useProposalResourceGetObservingProposalTitle(
-            {pathParams: {proposalCode: Number(selectedProposalCode)}}
-            );
+    const {data: titleData, error: titleError, isLoading: titleLoading} =
+        useProposalResourceGetObservingProposalTitle(
+        {pathParams: {proposalCode: Number(selectedProposalCode)}}
+        );
 
-        if (observationsError) {
-            return (
-                <pre>{getErrorMessage(observationsError)}</pre>
-            );
-        }
-
-        if (targetsError) {
-            return (
-                <pre>{getErrorMessage(targetsError)}</pre>
-            )
-        }
-
-        if (titleError) {
-            return (
-                <pre>{getErrorMessage(titleError)}</pre>
-            )
-        }
-
+    if (observationsError) {
         return (
-            <div>
-                <h3>
-                    { titleLoading ?
-                        <Badge size={"xl"} radius={0}>...</Badge> :
-                        <Badge size={"xl"} radius={0}>{titleData}</Badge>
-                    }
-                    : Observations
-                </h3>
-
-                {observationsLoading ? (`Loading...`) :
-                    <Table>
-                        { observationTableHeader() }
-                        <Table.Tbody>
-                        {
-                            observations?.map((observation) => {
-                                return (
-                                    <ObservationRow id={observation.dbid!} key={observation.dbid!} />
-                                )
-                            })
-                        }
-                        </Table.Tbody>
-                    </Table>
-                }
-
-                <Space h={"xs"}/>
-
-                <Group justify={'flex-end'}>
-                    {targetsLoading ? (`Loading...`) :
-                        targets!.length > 0 ?
-                            <ObservationsNewModal/> :
-                            <Group>
-                            <Text c={"yellow"} mr={-5}>
-                                To create an observation please add at least one
-                            </Text>
-                            <Button
-                                variant={"subtle"}
-                                p={5}
-                                ml={-5}
-                                to={"../proposal/" + selectedProposalCode + "/targets"}
-                                component={Link}
-                            >
-                                target
-                            </Button>
-                            </Group>
-
-                    }
-                </Group>
-            </div>
+            <pre>{getErrorMessage(observationsError)}</pre>
         );
     }
 
+    if (targetsError) {
+        return (
+            <pre>{getErrorMessage(targetsError)}</pre>
+        )
+    }
+
+    if (titleError) {
+        return (
+            <pre>{getErrorMessage(titleError)}</pre>
+        )
+    }
+
+    return (
+        <div>
+            <h3>
+                { titleLoading ?
+                    <Badge size={"xl"} radius={0}>...</Badge> :
+                    <Badge size={"xl"} radius={0}>{titleData}</Badge>
+                }
+                : Observations
+            </h3>
+
+            {observationsLoading ? (`Loading...`) :
+                <Table>
+                    { observationTableHeader() }
+                    <Table.Tbody>
+                    {
+                        observations?.map((observation) => {
+                            return (
+                                <ObservationRow id={observation.dbid!} key={observation.dbid!} />
+                            )
+                        })
+                    }
+                    </Table.Tbody>
+                </Table>
+            }
+
+            <Space h={"xs"}/>
+
+            <Group justify={'flex-end'}>
+                {targetsLoading ? (`Loading...`) :
+                    targets!.length > 0 ?
+                        <ObservationEditModal
+                            observation={undefined}
+                            newObservation={true}
+                        /> :
+                        <Group>
+                        <Text c={"yellow"} mr={-5}>
+                            To create an observation please add at least one
+                        </Text>
+                        <Button
+                            variant={"subtle"}
+                            p={5}
+                            ml={-5}
+                            to={"../proposal/" + selectedProposalCode + "/targets"}
+                            component={Link}
+                        >
+                            target
+                        </Button>
+                        </Group>
+                }
+            </Group>
+        </div>
+    );
 }
 
 export default ObservationsPanel
