1e6544759fa5d6ff04002d83da5e921979c2f894
Darren Walker <darrenwalker1@hotmail.co.uk>
2024-03-11 15:39:36 +0000

    use 'Justification' as form fields type, make the text area and select box react to viewport width

--- src/main/webui/src/justifications/JustificationsPanel.tsx
+++ src/main/webui/src/justifications/JustificationsPanel.tsx
@@ -1,31 +1,32 @@
 import {ReactElement, useEffect, useState} from "react";
-import {Box, Container, Group, Select, Textarea} from "@mantine/core";
+import {Box, Container, Grid, Select, Textarea} from "@mantine/core";
 import {useParams} from "react-router-dom";
 import {useProposalResourceGetJustification} from "../generated/proposalToolComponents.ts";
 import {JSON_SPACES, MAX_CHARS_FOR_INPUTS} from "../constants.tsx";
 import {useForm, UseFormReturnType} from "@mantine/form";
+import {Justification, TextFormats} from "../generated/proposalToolSchemas.ts";
 
 
 interface JustificationsFormValues {
-    scientific: {
-        text: string,
-        textFormat: string
-    },
-    technical: {
-        text: string,
-        textFormat: string
-    }
+    scientific: Justification,
+    technical: Justification
 }
 
 type Which = 'scientific' | 'technical';
 
 
 export default function JustificationsPanel() : ReactElement {
+
+    const DEFAULT_JUSTIFICATION : Justification = {text: "", format: "ASCIIDOC" };
+
+
     const { selectedProposalCode } = useParams();
 
     //we need these states to set the texts correctly on first visit to the JustificationsPanel
-    const [_scientificText, setScientificText] = useState("");
-    const [_technicalText, setTechnicalText] = useState("");
+    const [_scientificJustification, setScientificJustification]
+        = useState<Justification>(DEFAULT_JUSTIFICATION);
+    const [_technicalJustification, setTechnicalJustification]
+        = useState<Justification>(DEFAULT_JUSTIFICATION);
 
     const {
         data : scientific,
@@ -54,39 +55,35 @@ export default function JustificationsPanel() : ReactElement {
     //required to set the texts correctly on first time visit
     useEffect(() => {
         if(scientificStatus === 'success') {
-            setScientificText(scientific?.text ?? "");
-            form.values.scientific.text = scientific?.text ?? "";
+            setScientificJustification(scientific ?? DEFAULT_JUSTIFICATION);
+            form.values.scientific = scientific ?? DEFAULT_JUSTIFICATION;
         }
         if(technicalStatus === 'success') {
-            setTechnicalText(technical?.text ?? "");
-            form.values.technical.text = technical?.text ?? "";
+            setTechnicalJustification(technical ?? DEFAULT_JUSTIFICATION);
+            form.values.technical = technical ?? DEFAULT_JUSTIFICATION
         }
     }, [scientificStatus, scientific, technicalStatus, technical]);
 
     const form: UseFormReturnType<JustificationsFormValues> =
         useForm<JustificationsFormValues>({
             initialValues: {
-                scientific: {
-                    text: scientific ? scientific.text! : "",
-                    textFormat: scientific ? scientific.format! : "ASCIIDOC"
-                },
-                technical: {
-                    text: technical ? technical.text! : "",
-                    textFormat: scientific ? scientific.format! : "ASCIIDOC"
-                }
+                scientific: scientific ? scientific : DEFAULT_JUSTIFICATION,
+                technical: technical ? technical : DEFAULT_JUSTIFICATION
             },
             validate: {
                 scientific : {
-                    text: (value: string) =>
-                        (value === "" ? "Text cannot be empty for a Scientific Justification" : null),
-                    textFormat: (value: string) =>
+                    text: (value: string | undefined) =>
+                        (value === "" || value === undefined ?
+                            "Text cannot be empty for a Scientific Justification" : null),
+                    format: (value: TextFormats | undefined ) =>
                         (value !== "LATEX" && value !== "RST" && value !== "ASCIIDOC" ?
                             'Text format one of: LATEX, RST, or ASCIIDOC' : null)
                 },
                 technical : {
-                    text: (value: string) =>
-                        (value === "" ? "Text cannot be empty for a Technical Justification" : null),
-                    textFormat: (value: string) =>
+                    text: (value: string | undefined) =>
+                        (value === "" || value === undefined ?
+                            "Text cannot be empty for a Technical Justification" : null),
+                    format: (value: TextFormats | undefined) =>
                         (value !== "LATEX" && value !== "RST" && value !== "ASCIIDOC" ?
                             'Text format one of: LATEX, RST, or ASCIIDOC' : null)
                 }
@@ -123,7 +120,7 @@ export default function JustificationsPanel() : ReactElement {
                 description={
                     MAX_CHARS_FOR_INPUTS
                     - (which === "scientific" ?
-                        form.values.scientific.text.length : form.values.technical.text.length)
+                        form.values.scientific.text!.length : form.values.technical.text!.length)
                     + "/" + String(MAX_CHARS_FOR_INPUTS)
                 }
                 inputWrapperOrder={['label', 'error', 'input', 'description']}
@@ -143,7 +140,7 @@ export default function JustificationsPanel() : ReactElement {
                     {value: 'RST', label: 'RST'},
                     {value: 'ASCIIDOC', label: 'ASCII'}
                 ]}
-                {...form.getInputProps(which + '.textFormat')}
+                {...form.getInputProps(which + '.format')}
             />
         )
     }
@@ -152,17 +149,23 @@ export default function JustificationsPanel() : ReactElement {
     const handleSubmit = () =>{}
 
     return (
-        <Container>
+        <Container fluid>
             {scientificIsLoading && technicalIsLoading ? (`Loading justifications...`) :
                 <form onSubmit={handleSubmit}>
-                    <Group>
-                        {JustificationTextArea('scientific')}
-                        {SelectTextFormat('scientific')}
-                    </Group>
-                    <Group>
-                        {JustificationTextArea('technical')}
-                        {SelectTextFormat('technical')}
-                    </Group>
+                    <Grid span={10} grow>
+                        <Grid.Col span={{base: 6, md: 8, lg: 9}}>
+                            {JustificationTextArea('scientific')}
+                        </Grid.Col>
+                        <Grid.Col span={{base: 4, md: 2, lg: 1}}>
+                            {SelectTextFormat('scientific')}
+                        </Grid.Col>
+                        <Grid.Col span={{base: 6, md: 8, lg: 9}}>
+                            {JustificationTextArea('technical')}
+                        </Grid.Col>
+                        <Grid.Col span={{base: 4, md: 2, lg: 1}}>
+                            {SelectTextFormat('technical')}
+                        </Grid.Col>
+                    </Grid>
                 </form>
             }
         </Container>
