6a5fe5ace586dd472dd36fed56da68dda7536380
Allan England <allan.england@manchester.ac.uk>
2024-03-15 10:10:43 +0000

    Remove document resize slider

--- src/main/webui/src/ColourSchemeToggle.tsx
+++ src/main/webui/src/ColourSchemeToggle.tsx
@@ -1,6 +1,5 @@
 import {useMantineColorScheme, Switch, useMantineTheme} from '@mantine/core';
 import {IconSun, IconMoonStars} from '@tabler/icons-react';
-import { Slider } from '@mantine/core';
 import { STROKE } from './constants.tsx';
 
 export function SwitchToggle() {
@@ -23,16 +22,6 @@ export function SwitchToggle() {
                     color={GRAY}
                     size="1.25em"
                     stroke={STROKE}/>}
-            />
-            <Slider miw={120}
-                label={null}
-                showLabelOnHover={false}
-                defaultValue={100}
-                min={70}
-                max={130}
-                onChange={(value) => {
-                    document.documentElement.style.fontSize = `${value}%`;
-                }}
             />
         </>
     );
