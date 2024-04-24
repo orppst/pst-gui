6583d0612f9f5956962766c7d4ec75e677439943
Allan England <allan.england@manchester.ac.uk>
2024-04-24 10:41:31 +0100

    Form to change cycle title

--- src/main/webui/src/ProposalManagerView/proposalCycle/title.tsx
+++ src/main/webui/src/ProposalManagerView/proposalCycle/title.tsx
@@ -1,10 +1,100 @@
-import {ReactElement} from "react";
-import {Container, Text} from "@mantine/core";
+import {ReactElement, useEffect, useState} from "react";
+import {Box, Container, Text, TextInput} from "@mantine/core";
+import {useParams} from "react-router-dom";
+import {
+    fetchProposalCyclesResourceReplaceCycleTitle,
+    ProposalCyclesResourceReplaceCycleTitleVariables,
+    useProposalCyclesResourceGetProposalCycleTitle
+} from "../../generated/proposalToolComponents.ts";
+import {useForm} from "@mantine/form";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {HEADER_FONT_WEIGHT, JSON_SPACES, MAX_CHARS_FOR_INPUTS} from "../../constants.tsx";
+import MaxCharsForInputRemaining from "../../commonInputs/remainingCharacterCount.tsx";
+import {SubmitButton} from "../../commonButtons/save.tsx";
+
+const titleFormJSON =  {
+    initialValues: {title: "Loading..."},
+    validate: {
+        title: (value : string) => (
+            value.length < 1 ? 'Title cannot be blank' : null)
+    }
+};
 
 export default function CycleTitlePanel() : ReactElement {
+    const {selectedCycleCode} = useParams();
+    const [submitting, setSubmitting] = useState(false);
+    const [title, setTitle] = useState("")
+    const { data, error, isLoading, status } =
+        useProposalCyclesResourceGetProposalCycleTitle(
+            {pathParams: {cycleCode: Number(selectedCycleCode)}}
+        );
+
+    const form = useForm(titleFormJSON);
+
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation({
+        mutationFn: () => {
+            //IMPL the code generator does not create the correct type
+            // signature for API calls where the body is plain text.
+            const newTitle : ProposalCyclesResourceReplaceCycleTitleVariables = {
+                pathParams: {cycleCode: Number(selectedCycleCode)},
+                body: title,
+                // @ts-ignore
+                headers: {"Content-Type": "text/plain"}
+            }
+            return fetchProposalCyclesResourceReplaceCycleTitle(newTitle);
+        },
+        onMutate: () => {
+            setSubmitting(true);
+        },
+        onError: () => {
+            console.log("An error occurred trying to update the title")
+        },
+        onSuccess: () => {
+            //IMPL this is slightly limiting the invalidation -
+            // some things should be ok still (users etc).
+            queryClient.invalidateQueries(["pst","api","proposals"])
+                .then(()=> setSubmitting(false))
+        },
+    })
+
+    useEffect(() => {
+        if (status === 'success') {
+            setTitle(data as unknown as string);
+            form.values.title = data as unknown as string;
+        }
+    }, [status,data]);
+
+    if (error) {
+        return (
+            <Box>
+                <pre>{JSON.stringify(error, null, JSON_SPACES)}</pre>
+            </Box>
+        );
+    }
+
+    const updateTitle = form.onSubmit((val) => {
+        form.validate();
+        setTitle(val.title);
+        mutation.mutate();
+    });
+
     return (
         <Container fluid>
-            <Text>WIP: this is where you view/edit the proposal cycle title</Text>
+            <Text fz="lg" fw={HEADER_FONT_WEIGHT}>Update title</Text>
+            { isLoading ? ("Loading..") :
+                submitting ? ("Submitting..."):
+                    <form onSubmit={updateTitle}>
+                        <TextInput name="title"
+                                   maxLength={MAX_CHARS_FOR_INPUTS}
+                                   {...form.getInputProps('title')}/>
+                        <MaxCharsForInputRemaining length={form.values.title.length} />
+                        <br/>
+                        <SubmitButton toolTipLabel={"save title"}
+                                      label={"Save"}/>
+                    </form>
+            }
         </Container>
     )
 }
