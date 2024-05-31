d2b32e4b8f51754f2d533b3611bc8d7b10c830fe
Darren Walker <darrenwalker1@hotmail.co.uk>
2024-05-31 12:14:03 +0100

    visual improvements of the left hand navigation for proposals (highlighting mostly)

--- src/main/webui/src/ProposalList.tsx
+++ src/main/webui/src/ProposalList.tsx
@@ -1,5 +1,5 @@
 import {ReactElement, useState} from "react";
-import {Accordion, Group, NavLink} from "@mantine/core";
+import {Accordion, NavLink, useMantineColorScheme, useMantineTheme} from "@mantine/core";
 import {
     IconChartLine,
     IconFileCheck, IconFiles, IconGeometry,
@@ -10,7 +10,7 @@ import {
     IconUfo,
     IconUsersGroup
 } from "@tabler/icons-react";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {useProposalResourceGetProposals} from "./generated/proposalToolComponents.ts";
 import {ProposalSynopsis} from "./generated/proposalToolSchemas.ts";
 
@@ -19,6 +19,14 @@ export function ProposalList(props:{proposalTitle: string, investigatorName:stri
 
     const [accordionValue, setAccordionValue]
         = useState<string | null>(null);
+
+    const [active, setActive] = useState("");
+
+    const {selectedProposalCode} = useParams();
+
+    const theme = useMantineTheme();
+    const {colorScheme} = useMantineColorScheme();
+
     const result = useProposalResourceGetProposals(
         { queryParams: {
                 title: "%" + props.proposalTitle + "%",
@@ -29,6 +37,119 @@ export function ProposalList(props:{proposalTitle: string, investigatorName:stri
             enabled: true,
         });
 
+    function ProposalItem(props:{proposal:ProposalSynopsis}): ReactElement {
+        const proposal = props.proposal
+        return (
+            <Accordion.Item
+                value={String(proposal.code)}
+                key={proposal.code}
+                bg={colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[1]}
+            >
+                <Accordion.Control>
+                    <NavLink to={"proposal/" + proposal.code}
+                             component={Link}
+                             label={proposal.title}
+                             leftSection={<IconLicense/>}
+                             variant={"subtle"}
+                             active={proposal.code === Number(selectedProposalCode)}
+                             onClick={()=>setActive("Overview" + proposal.code)}
+                             ml={-10}
+                    />
+                </Accordion.Control>
+                <Accordion.Panel>
+                    <NavLink to={"proposal/" + proposal.code}
+                             component={Link}
+                             key="Overview"
+                             label="Overview"
+                             leftSection={<IconUfo/>}
+                             active={"Overview" + proposal.code === active}
+                             onClick={()=>setActive("Overview" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/title"}
+                             component={Link}
+                             leftSection={<IconLetterT/>}
+                             label="Title"
+                             key="Title"
+                             active={"Title" + proposal.code === active}
+                             onClick={()=>setActive("Title" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/summary"}
+                             component={Link}
+                             leftSection={<IconLetterS/>}
+                             label="Summary"
+                             key="Summary"
+                             active={"Summary" + proposal.code === active}
+                             onClick={()=>setActive("Summary" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/investigators"}
+                             component={Link}
+                             leftSection={<IconUsersGroup/>}
+                             label="Investigators"
+                             key="Investigators"
+                             active={"Investigators" + proposal.code === active}
+                             onClick={()=>setActive("Investigators" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/justifications"}
+                             component={Link}
+                             leftSection={<IconFileCheck/>}
+                             label="Justifications"
+                             key="Justifications"
+                             active={"Justifications" + proposal.code === active}
+                             onClick={()=>setActive("Justifications" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/targets"}
+                             component={Link}
+                             leftSection={<IconTarget/>}
+                             label="Targets"
+                             key="Targets"
+                             active={"Targets" + proposal.code === active}
+                             onClick={()=>setActive("Targets" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/goals"}
+                             component={Link}
+                             leftSection={<IconChartLine/>}
+                             label="Technical Goals"
+                             key="Technical Goals"
+                             active={"Technical Goals" + proposal.code === active}
+                             onClick={()=>setActive("Technical Goals" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/observationFields"}
+                             component={Link}
+                             leftSection={<IconGeometry/>}
+                             label="Observation Fields"
+                             key="Observation Fields"
+                             active={"Observation Fields" + proposal.code === active}
+                             onClick={()=>setActive("Observation Fields" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/observations"}
+                             component={Link}
+                             leftSection={<IconTelescope/>}
+                             label="Observations"
+                             key="Observations"
+                             active={"Observations" + proposal.code === active}
+                             onClick={()=>setActive("Observations" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/documents"}
+                             component={Link}
+                             leftSection={<IconFiles/>}
+                             label="Documents"
+                             key="Documents"
+                             active={"Documents" + proposal.code === active}
+                             onClick={()=>setActive("Documents" + proposal.code)}
+                    />
+                    <NavLink to={"proposal/" + proposal.code + "/submit"}
+                             component={Link}
+                             leftSection={<IconSend/>}
+                             label="Submit"
+                             key="Submit"
+                             active={"Submit" + proposal.code === active}
+                             onClick={()=>setActive("Submit" + proposal.code)}
+                    />
+                </Accordion.Panel>
+            </Accordion.Item>)
+    }
+
+
     const proposalsList = result.data?.map((proposal) => {
         return  <ProposalItem key={proposal.code} proposal={proposal}/>
     })
@@ -37,6 +158,7 @@ export function ProposalList(props:{proposalTitle: string, investigatorName:stri
             value={accordionValue}
             onChange={setAccordionValue}
             variant={"filled"}
+            bg={colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[1]}
         >
             {proposalsList}
         </Accordion>
@@ -44,106 +166,4 @@ export function ProposalList(props:{proposalTitle: string, investigatorName:stri
 }
 
 
-    function ProposalItem(props:{proposal:ProposalSynopsis}): ReactElement {
-        const proposal = props.proposal
-        const [active, setActive] = useState("");
 
-        return (<Accordion.Item value={String(proposal.code)} key={proposal.code}>
-            <Accordion.Control>
-                <Group>
-                    <IconLicense/>
-                    {proposal.title}
-                </Group>
-            </Accordion.Control>
-            <Accordion.Panel>
-                <NavLink to={"proposal/" + proposal.code}
-                         component={Link}
-                         key="Overview"
-                         label="Overview"
-                         leftSection={<IconUfo/>}
-                         active={"Overview" + proposal.code === active}
-                         onClick={()=>setActive("Overview" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/title"}
-                         component={Link}
-                         leftSection={<IconLetterT/>}
-                         label="Title"
-                         key="Title"
-                         active={"Title" + proposal.code === active}
-                         onClick={()=>setActive("Title" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/summary"}
-                         component={Link}
-                         leftSection={<IconLetterS/>}
-                         label="Summary"
-                         key="Summary"
-                         active={"Summary" + proposal.code === active}
-                         onClick={()=>setActive("Summary" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/investigators"}
-                         component={Link}
-                         leftSection={<IconUsersGroup/>}
-                         label="Investigators"
-                         key="Investigators"
-                         active={"Investigators" + proposal.code === active}
-                         onClick={()=>setActive("Investigators" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/justifications"}
-                         component={Link}
-                         leftSection={<IconFileCheck/>}
-                         label="Justifications"
-                         key="Justifications"
-                         active={"Justifications" + proposal.code === active}
-                         onClick={()=>setActive("Justifications" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/targets"}
-                         component={Link}
-                         leftSection={<IconTarget/>}
-                         label="Targets"
-                         key="Targets"
-                         active={"Targets" + proposal.code === active}
-                         onClick={()=>setActive("Targets" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/goals"}
-                         component={Link}
-                         leftSection={<IconChartLine/>}
-                         label="Technical Goals"
-                         key="Technical Goals"
-                         active={"Technical Goals" + proposal.code === active}
-                         onClick={()=>setActive("Technical Goals" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/observationFields"}
-                         component={Link}
-                         leftSection={<IconGeometry/>}
-                         label="Observation Fields"
-                         key="Observation Fields"
-                         active={"Observation Fields" + proposal.code === active}
-                         onClick={()=>setActive("Observation Fields" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/observations"}
-                         component={Link}
-                         leftSection={<IconTelescope/>}
-                         label="Observations"
-                         key="Observations"
-                         active={"Observations" + proposal.code === active}
-                         onClick={()=>setActive("Observations" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/documents"}
-                         component={Link}
-                         leftSection={<IconFiles/>}
-                         label="Documents"
-                         key="Documents"
-                         active={"Documents" + proposal.code === active}
-                         onClick={()=>setActive("Documents" + proposal.code)}
-                />
-                <NavLink to={"proposal/" + proposal.code + "/submit"}
-                         component={Link}
-                         leftSection={<IconSend/>}
-                         label="Submit"
-                         key="Submit"
-                         active={"Submit" + proposal.code === active}
-                         onClick={()=>setActive("Submit" + proposal.code)}
-                />
-            </Accordion.Panel>
-        </Accordion.Item>)
-    }
