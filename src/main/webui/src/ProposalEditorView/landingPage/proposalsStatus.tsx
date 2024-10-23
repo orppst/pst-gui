13eddee2b8859616cd47b8099406166736c9400e
Allan England <allan.england@manchester.ac.uk>
2024-10-23 16:27:14 +0100

    Proposals list done, but submitted flag is always false - if set true the proposal is no longer visible in the GUI

--- src/main/webui/src/ProposalEditorView/landingPage/proposalsStatus.tsx
+++ src/main/webui/src/ProposalEditorView/landingPage/proposalsStatus.tsx
@@ -1,9 +1,53 @@
 import {PanelHeader} from "../../commonPanel/appearance.tsx";
+import {
+    useProposalResourceGetProposals
+} from "../../generated/proposalToolComponents.ts";
+import {List, Table} from "@mantine/core";
+import {randomId} from "@mantine/hooks";
+
+type ProposalRowProps = {
+    title: string
+    submitted: boolean
+}
+
+function ProposalsTableRow(props: ProposalRowProps) {
+    return <Table.Tr>
+        <Table.Td>{props.title}</Table.Td>
+        <Table.Td>{props.submitted?'Yes':'No'}</Table.Td>
+    </Table.Tr>
+
+}
 
 function ProposalsStatus () {
+    const {data, error, isLoading} = useProposalResourceGetProposals({});
+
+    if(error) {
+        return <><PanelHeader itemName={"Proposals"} panelHeading={"Unavailable"}/></>
+    }
+
+    const listProposals = () => {
+        if(data?.length == 0) {
+            return <List key={randomId()}>Please create a proposal</List>
+        }
+        return <Table>
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th>Title</Table.Th>
+                    <Table.Th>Submitted</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+                {data?.map((proposal) => {
+                    return <ProposalsTableRow key={proposal.code}
+                                              title={proposal.title!}
+                                              submitted={proposal.submitted!} />;
+                })}
+            </Table.Tbody>
+        </Table>
+    }
 
     return <><PanelHeader itemName={"Proposals"} panelHeading={"Status"}/>
-        A list of proposals you are an investigator on and their current status
+        {isLoading?'Loading...':listProposals()}
         </>
 
 }
