308e2ad4fa039df4b87424e9fd75752f4e0509c9
Allan England <allan.england@manchester.ac.uk>
2026-02-12 14:43:31 +0000

    Try a recorded test

--- src/main/webui/tests/Polaris.test.ts
+++ src/main/webui/tests/Polaris.test.ts
@@ -40,3 +40,30 @@ test('login as TAC Chair', async ({ page }) => {
     // Check we **can** see the TAC Management button
     await expect(page.getByRole('button', { name: 'TAC Management'})).toBeVisible()
 });
+
+test('random prerecorded test', async ({ page }) => {
+    await page.goto('http://localhost:8080/pst/gui/tool/');
+    await page.getByRole('textbox', { name: 'Username or email' }).click();
+    await page.getByRole('textbox', { name: 'Username or email' }).fill('tacchair');
+    await page.getByRole('textbox', { name: 'Password' }).fill('tacchair');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.getByRole('button', { name: 'TAC Management' }).click();
+    await page.getByRole('button', { name: 'Cycle 19' }).click();
+    await page.getByRole('link', { name: 'Details' }).click();
+    await page.getByRole('textbox', { name: 'Title' }).fill('A test cycle called number 19');
+    await page.getByRole('textbox', { name: 'Unique code' }).fill('EM0019');
+    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('link', { name: 'TAC and Reviewers' }).click();
+    await page.getByRole('button', { name: 'Add Reviewer' }).click();
+    await page.getByRole('textbox', { name: 'email' }).click();
+    await page.getByRole('textbox', { name: 'email' }).fill('reviewer@unreal.not.email');
+    await page.getByRole('textbox', { name: 'Name' }).click();
+    await page.getByRole('button', { name: 'Add' }).click();
+    await page.getByRole('link', { name: 'Assign Reviewers' }).click();
+    await page.getByRole('link', { name: 'Reviews' }).click();
+    await page.getByRole('link', { name: 'Accept Proposals' }).click();
+    await page.getByRole('button').nth(3).click();
+    await page.getByRole('menuitem', { name: 'Profile' }).click();
+    await page.getByRole('textbox', { name: 'Last Name' }).fill('Halley (Chair)');
+    await page.getByRole('button', { name: 'Save' }).first().click();
+});
