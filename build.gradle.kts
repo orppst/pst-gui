4781540ccbd8e442bb98023f83b0f7e0dbf7c5c3
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-03-17 12:57:04 +0000

    update to quarkus 3.32.3
    * also update the gradle wrapper to 9.4
    * remove some of the build logic platform complexity (at the expense of a bit more repetition perhaps)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,21 +5,22 @@ plugins {
 version = "1.0"
 
 dependencies {
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
-    implementation("io.quarkus:quarkus-rest-client-reactive-jackson")
-    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.3.10")
+    implementation("org.orph2020.pst:pst-lib")
+    implementation("io.quarkus:quarkus-rest-jackson")
+    implementation("io.quarkus:quarkus-rest-client-jackson")
+    implementation("io.quarkiverse.quinoa:quarkus-quinoa:2.7.2")
     implementation("io.quarkus:quarkus-reactive-routes")
     implementation("io.quarkus:quarkus-websockets")
     implementation("io.quarkus:quarkus-smallrye-jwt")
     implementation("io.quarkus:quarkus-oidc")
-//    implementation("io.quarkus:quarkus-oidc-client-reactive-filter")  // https://quarkus.io/guides/security-openid-connect-client
+//    implementation("io.quarkus:quarkus-rest-client-oidc-filter")  // https://quarkus.io/guides/security-openid-connect-client
 //    implementation("io.quarkus:quarkus-oidc-token-propagation-reactive")  // https://quarkus.io/guides/security-openid-connect
 
 
 //    implementation("io.quarkus:quarkus-keycloak-admin-client") // https://quarkus.io/guides/security-keycloak-admin-client
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.quarkus:quarkus-resteasy-reactive")
+    implementation("io.quarkus:quarkus-rest")
     testImplementation("io.rest-assured:rest-assured")
-    testImplementation("io.quarkiverse.quinoa:quarkus-quinoa-testing:1.2.9")
+    testImplementation("io.quarkiverse.quinoa:quarkus-quinoa-testing:2.7.2")
 }
 
